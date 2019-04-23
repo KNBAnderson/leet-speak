074c85af5e14189fe753b0204a9b5bdce7eefd6a
Katlin Anderson and Liz Kelley <katlin@>
2019-04-23 16:51:37 -0700

    add method to translate to Leet Speak

--- Leet.Tests/ModelTests/LeetTest.cs
+++ Leet.Tests/ModelTests/LeetTest.cs
@@ -7,10 +7,46 @@ namespace Leet.Tests
   public class LeetTest
   {
     [TestMethod]
+    public void MakeLeetSpeak_LetterIsNotLeet_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual('d', testLeetSpeak.MakeLeetSpeak('d'));
+    }
+    [TestMethod]
     public void MakeLeetSpeak_LetterIsE_True()
     {
       LeetSpeak testLeetSpeak = new LeetSpeak();
       Assert.AreEqual('3', testLeetSpeak.MakeLeetSpeak('e'));
+    }
+    [TestMethod]
+    public void MakeLeetSpeak_LetterIsO_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual('0', testLeetSpeak.MakeLeetSpeak('o'));
+    }
+    [TestMethod]
+    public void MakeLeetSpeak_LetterIsI_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual('1', testLeetSpeak.MakeLeetSpeak('I'));
+    }
+    [TestMethod]
+    public void MakeLeetSpeak_LetterIsT_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual('7', testLeetSpeak.MakeLeetSpeak('T'));
+    }
+    [TestMethod]
+    public void AssessWord_WordIsLeet_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual("y0u", testLeetSpeak.AssessWord("you"));
+    }
+    [TestMethod]
+    public void AssessWord_SWordIsLeet_True()
+    {
+      LeetSpeak testLeetSpeak = new LeetSpeak();
+      Assert.AreEqual("sn3k", testLeetSpeak.AssessWord("snek"));
     }
   }
 }
--- Leet/Models/Leet.cs
+++ Leet/Models/Leet.cs
@@ -8,7 +8,66 @@ namespace Leet
       {
         return '3';
       }
+      else if (letter == 'o' || letter == 'O')
+      {
+        return '0';
+      }
+      else if (letter == 'I')
+      {
+        return '1';
+      }
+      else if (letter == 't' || letter == 'T')
+      {
+        return '7';
+      }
+      else if (letter == 's' || letter == 'S')
+      {
+        return 'Z';
+      }
       return letter;
     }
+
+    public string AssessWord(string word)
+    {
+      string newWord = "";
+      char[] charArray = word.ToCharArray();
+      for (int index = 0; index <= charArray.Length - 1; index++)
+      {
+        if (index == 0 && (charArray[index] == 's' || charArray[index] == 'S'))
+        {
+          newWord += charArray[index].ToString();
+        }
+        else
+        {
+          newWord += MakeLeetSpeak(charArray[index]).ToString();
+        }
+      }
+      return newWord;
+    }
+
+    public string BreakApartString(string words)
+    {
+      string stupidWords = "";
+      string[] wordArray = words.Split(' ');
+
+      foreach (string word in wordArray)
+      {
+        stupidWords += AssessWord(word);
+      }
+        return stupidWords;
+    }
   }
+
 }
+
+
+
+
+
+//char[] chars = word.ToCharArray();
+//for loop through chars.lenght
+
+//  if i == 0 && chars[i] == 's' || 'S'
+//     return chars[0]
+//  else
+//  MakeLeetSpeak(chars[i])
